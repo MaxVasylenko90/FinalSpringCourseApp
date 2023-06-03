0aac0e9adbbed7c73206aed41f4d7784a3e2b921
Maksym Vasylenko <vasilenkomagic@gmail.com>
2023-06-03 12:31:48 +0200

    nothing new, just some issues with db was solved

--- src/main/java/ua/vasylenko/SensorApp/services/SensorService.java
+++ src/main/java/ua/vasylenko/SensorApp/services/SensorService.java
@@ -1,16 +1,19 @@
 package ua.vasylenko.SensorApp.services;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ua.vasylenko.SensorApp.models.Sensor;
 import ua.vasylenko.SensorApp.repositories.SensorRepository;
 
 @Service
+@Transactional(readOnly = true)
 public class SensorService {
     private final SensorRepository sensorRepository;
 
     public SensorService(SensorRepository sensorRepository) {
         this.sensorRepository = sensorRepository;
     }
+    @Transactional
     public void registration(Sensor sensor) {
         sensorRepository.save(sensor);
     }
